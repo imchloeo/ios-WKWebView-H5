aef249044685414d308a98b402a3be67d37fc168
我没有猫 <ermao@mengdeMacBook-Pro.local>
2020-11-06 20:48:06 +0800

    init commit

---
+++ swift-H5/swift_H5App.swift
@@ -0,0 +1,17 @@
+//
+//  swift_H5App.swift
+//  swift-H5
+//
+//  Created by 我没有猫 on 2020/11/6.
+//
+
+import SwiftUI
+
+@main
+struct swift_H5App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
